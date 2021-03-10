b816f96d6fdd5dc7e42804cb6722a8b4669189ad
Michael Hsieh <mdhsieh8@gmail.com>
2021-03-10 15:04:09 -0800

    feat: Get biggest contour from a single image frame.
    Commented out function that directly uses image frames from video caputre.
    Using a single image frame since trying to find ROI image from video capture
    is crashing the program.

--- Homework2/hw2.py
+++ Homework2/hw2.py
@@ -36,6 +36,8 @@
 from tensorflow.keras.models import load_model
 import cv2
 
+# previous video capture directly using frames from live video
+'''
 # video capture
 import numpy as np
 
@@ -53,6 +55,7 @@ def video_capture():
         cv2.imshow('gray scale',gray)
         cv2.imshow('Canny edges',edges)
         
+        # Show Canny edges on original image
         # for bitwise_or the shape needs to be the same, so we need to add an axis, 
         # since our input image has 3 axis while the canny output image has only one 2 axis
         out = np.bitwise_or(frame, edges[:,:,np.newaxis])
@@ -63,15 +66,13 @@ def video_capture():
         # Use a copy of the image e.g. edged.copy() 
         # since findContours alters the image 
         contours, hierarchy = cv2.findContours(edges_copy,  
-            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-          
-        cv2.imshow('Canny edges after contouring', edges_copy) 
-          
-        # print("Number of contours found: " + str(len(contours))) 
-          
-        # Draw all contours 
-        # -1 signifies drawing all contours 
-        cv2.drawContours(frame, contours, -1, (0, 255, 0), 3) 
+            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        
+        # find the biggest countour (c) by the area
+        c = max(contours, key = cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(c)
+        # draw the biggest contour (c) in green
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
           
         cv2.imshow('Contours', frame)
 
@@ -81,9 +82,8 @@ def video_capture():
      
     cap.release()
     cv2.destroyAllWindows()
-    
-video_capture()
-      
+'''
+          
 # make a prediction for a new image.
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -115,7 +115,7 @@ imgToArr = load_image('sample_image.png')
 y_pred = model.predict_classes(imgToArr)
 print(y_pred)
 
-'''
+# Test code
 # draw bounding box with predicted digit caption text
 
 # find contours of a binary image
@@ -137,4 +137,131 @@ imageResized = cv2.resize(image, (760, 540))
 cv2.imshow('contours', imageResized)
 # Press q to quit
 cv2.waitKey(0) & 0xFF == ord('q')
-'''
+
+
+# video capture saved as image frames, then image frames processed
+import numpy as np
+
+# output_video_name: Name of the video saved from capture
+def capture_video(output_video_name):
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+    cap = cv2.VideoCapture(0)
+
+    while(cap.isOpened()):
+
+        ret, frame = cap.read() 
+
+        # write the image frame
+        out.write(frame)
+
+        cv2.imshow('original',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# Get image frames from captured video
+# input_video_name: Name of the video to save image frames from
+def save_image_frames(input_video_name):
+    videoName = "output-3.avi"
+    cap = cv2.VideoCapture(videoName)
+    ret, frame = cap.read()
+        
+    count = 0
+    while success:
+      # save 1 frame per second
+      cap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+      # save frame as JPEG file      
+      cv2.imwrite("frame%d.jpg" % count, frame)
+      ret, frame = cap.read()
+      print('Read a new frame: ', ret)
+      count += 1    
+
+# image_name: Image name
+# returns: The image
+def get_orig_image(image_name):
+    img = cv2.imread(image_name, cv2.IMREAD_COLOR)
+    return img
+      
+# Convert image frame to grayscale
+# image_name: Image name
+# returns: Grayscale image
+def convert_image_to_grayscale(image_name):
+    img = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+    return img
+
+# gray_image: Grayscale image
+# returns: Canny edge image, which is black and white
+def get_canny_edges(gray_image):
+    edges = cv2.Canny(gray_image,100,200)
+    return edges
+   
+# img: The original image
+# canny: Canny edges image 
+# returns: Original image with Canny edges drawn over it
+def get_orig_image_with_canny_edges(img, canny):
+    # for bitwise_or the shape needs to be the same, so we need to add an axis, 
+    # since our input image has 3 axis while the canny output image has only one 2 axis
+    out = np.bitwise_or(img, canny[:,:,np.newaxis])
+    return out
+
+# Show the given image in a window with the given window name.
+# window_name: Name of window image displayed in
+# image: Image to show
+# def show_image(window_name, image):
+    # cv2.imshow(window_name, image)
+    
+# edges: Canny edges image
+# returns: Contours found
+def find_contours(edges):
+    # Finding Contours 
+    edges_copy = edges.copy()
+    # Use a copy of the image e.g. edged.copy() 
+    # since findContours alters the image 
+    contours, hierarchy = cv2.findContours(edges_copy,  
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours
+    
+# image: Original image frame
+# contours: countours found from Canny edge image  
+# return: Original image with all countours drawn on it
+def find_all_contours(image, contours): 
+    return cv2.drawContours(image, contours, -1, (0, 255, 0), 3) 
+
+# image: Original image frame
+# contours: countours found from Canny edge image    
+def find_biggest_contour(image, contours):
+    # find the biggest countour (c) by the area
+    c = max(contours, key = cv2.contourArea)
+    x,y,w,h = cv2.boundingRect(c)
+    # draw the biggest contour (c) in green
+    image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+    return image
+
+# Get canny edge from one image frame
+IMAGE_FRAME_NAME = "frame10.jpg"
+img = get_orig_image(IMAGE_FRAME_NAME)
+cv2.imshow("original", img)
+gray_img = convert_image_to_grayscale(IMAGE_FRAME_NAME)
+cv2.imshow("gray_image", gray_img)
+canny_img = get_canny_edges(gray_img)
+cv2.imshow("Canny edges", canny_img)
+img_with_canny_edges = get_orig_image_with_canny_edges(img, canny_img)
+cv2.imshow("orig with edges", img_with_canny_edges)
+# Find and draw contours using Canny edges image
+contours = find_contours(canny_img)
+# img_with_all_contours = find_all_contours(img, contours)
+# cv2.imshow("orig with all contours", img_with_all_contours)
+img_biggest_contour = find_biggest_contour(img, contours)
+cv2.imshow("orig with biggest contour", img_biggest_contour)
+# Press any key to close windows
+# waits indefinitely for a key stroke
+k = cv2.waitKey(0) & 0xFF
+cv2.destroyAllWindows()
